913e58ff1b4f69ea8123dfe0f53cd317ce5f9e40
hoonDesk <hoonDesk@61.73.35.243>
2019-12-01 21:28:32 +0900

    REST API 적용중

--- src/main/java/com/weeks/controller/PostController.java
+++ src/main/java/com/weeks/controller/PostController.java
@@ -4,16 +4,21 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
+@RequestMapping(value = "/item")
 public class PostController {
 
 	private static final Logger logger = LoggerFactory.getLogger(MainController.class);
 
 	@RequestMapping(value = "/enroll", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
+		
+		logger.info("hello write");
 		
 		return "write";
 	}
